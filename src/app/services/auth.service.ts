12f8354b9ec0b630d5b00fbe5f3ef1db377d4138
alderjunio <alder.junio@gmail.com>
2025-10-17 13:35:54 -0400

    criado os componentes de services e guards

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3333/api/usuarios'; // Ajuste se necessário
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, senha: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, senha });
+  }
+
+  setSession(token: string, usuario: any) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  getUsuario() {
+    const user = localStorage.getItem('usuario');
+    return user ? JSON.parse(user) : null;
+  }
+
+  logout() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('usuario');
+
+}
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+}
